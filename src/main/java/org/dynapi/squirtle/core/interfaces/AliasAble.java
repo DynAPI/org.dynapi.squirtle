6cbc5befccad1ad9f3956c6cee2e81732daa55d9
playerg9 <g9yout@gmail.com>
2024-08-21 18:04:45 +0200

    code reformat

--- src/main/java/org/dynapi/squirtle/core/interfaces/AliasAble.java
+++ src/main/java/org/dynapi/squirtle/core/interfaces/AliasAble.java
@@ -2,15 +2,19 @@ package org.dynapi.squirtle.core.interfaces;
 
 public interface AliasAble {
     String getAlias();
-    void setAlias(String alias);
 
-    // weird syntax. I know. But this basically means `as` returns itself
+    /**
+     * @see AliasAble#as(String)
+     */
+    void setAlias(String alias);
 
     /**
      * changes the alias and returns itself for chained building
      * @param alias new alias
+     * @see AliasAble#setAlias(String)
      */
     @SuppressWarnings("unchecked")
+    // weird syntax. I know. But this basically means `as` returns itself
     default <T extends AliasAble> T as(String alias) {
         setAlias(alias);
         return (T) this;
